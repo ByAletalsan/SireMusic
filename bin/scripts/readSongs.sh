5f0cf8d8f9aa0391b3dd2f6104092ac41a309784
byaletalsan <byaletalsan@gmail.com>
2023-11-11 22:45:39 +0100

    SireMusic 7.1.0

--- bin/scripts/readSongs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-
-var=$(ls musica)
-
-echo $var > bin/data/temp.txt
-
-exit
---
+++ instalador.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "\033[38;2;173;216;230mInstalando actualizaciones...\033[0m"
+sudo apt-get -y update
+echo "\033[38;2;144;238;144mActualizaciones instaladas!\033[0m"
+echo "\033[38;2;173;216;230mInstalando reproductor...\033[0m"
+sudo apt update -y && sudo apt install sox -y
+sudo apt-get install libsox-fmt-all
+echo "\033[38;2;144;238;144mReproductor instalado!\033[0m"
+echo "\033[38;2;173;216;230mInstalando constructor del programa...\033[0m"
+sudo apt-get install build-essential
+echo "\033[38;2;144;238;144mInstalado constructor!\033[0m"
+echo "\033[38;2;173;216;230mInstalando herramientas necesarias...\033[0m"
+sudo apt-get install git
+sudo apt-get install evince
+echo "\033[38;2;144;238;144mHerramientas instaladas!\033[0m"
+echo "\033[38;2;173;216;230mCreando programa...\033[0m"
+if [ ! -d "SireMusic" ]; then
+	sudo git clone https://github.com/ByAletalsan/SireMusic.git
+	sudo rm -rf SireMusic/instalador.sh
+	sudo mkdir -p SireMusic/new_songs
+	sudo mkdir -p SireMusic/discoteca
+	sudo mkdir -p SireMusic/bin/music/channel_0
+	sudo mkdir -p SireMusic/bin/music/channel_1
+	sudo mkdir -p  SireMusic/bin/music/channel_2
+	sudo chmod 777 SireMusic
+	sudo chmod 777 SireMusic/*
+	sudo chmod 777 SireMusic/bin/*
+	sudo chmod 777 SireMusic/bin/data/*
+	sudo chmod 777 SireMusic/bin/scripts/*
+	sudo chmod 777 SireMusic/bin/music/*
+else
+	echo "\033[38;2;173;216;230mSireMusic detectado! Quieres actualizarlo [se conservaran las canciones] (yes, no):\033[0m"
+	read opcion
+	if [ "$opcion" = "yes" ] || [ "$opcion" = "y" ]; then
+		sudo mv SireMusic/bin/music music
+		sudo rm -rf SireMusic
+		sudo git clone https://github.com/ByAletalsan/SireMusic.git
+		sudo rm -rf SireMusic/instalador.sh
+		sudo mkdir -p SireMusic/new_songs
+		sudo mkdir -p SireMusic/discoteca
+		sudo mkdir -p SireMusic/bin/music/channel_0
+		sudo mkdir -p SireMusic/bin/music/channel_1
+		sudo mkdir -p  SireMusic/bin/music/channel_2
+		sudo chmod 777 SireMusic
+		sudo chmod 777 SireMusic/*
+		sudo chmod 777 SireMusic/bin/*
+		sudo chmod 777 SireMusic/bin/data/*
+		sudo chmod 777 SireMusic/bin/scripts/*
+		sudo mv music SireMusic/bin/music
+	fi
+fi
+sudo SireMusic/make
+echo "\033[38;2;144;238;144mYa puedes ejecutar el programa principal!!\033[0m"
+exit
